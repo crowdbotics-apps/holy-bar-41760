ed45b16f7ddb257fda595911dc792041ae651ce4
Crowdbotics <team@crowdbotics.com>
2023-06-27 15:27:31 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "holy-bar-41760",
+  url: "https://holy-bar-41760.botics.co",
+  api: "https://holy-bar-41760.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
